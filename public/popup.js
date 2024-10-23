a9caf01b07c7075e315b94f4a69f07cf5ebdd9e7
evywang <evywang@umich.edu>
2024-10-22 22:35:15 -0400

    scrapper

--- public/popup.js
+++ public/popup.js
@@ -1,1 +1,25 @@
-console.log('This is a popup!');
+
+
+let priceScrapper = document.getElementById('convertBtn')
+priceScrapper.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({active:true, currentWindow:true}); 
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: getSpanText,
+    });
+
+})
+
+// Function to get text content of a span element with class "hello"
+function getSpanText() {
+    let wholePrice = document.querySelector('span.a-price-whole');
+    if (wholePrice) {
+        const nestedElement = wholePrice.querySelector('.a-price-decimal');
+        if (nestedElement) {
+          nestedElement.remove();
+        }
+    }
+    let fractionPrice = document.querySelector('span.a-price-fraction');
+    alert(wholePrice.innerHTML + " " + fractionPrice.innerHTML);
+
+};
