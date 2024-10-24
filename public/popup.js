e2085ef1b1f1b36f17374d46bbb8c1dfeb6ddec7
Sidhant Roymoulik <sidhant.roymoulik@gmail.com>
2024-10-24 16:56:53 -0400

    Co-authored-by: Evan Wang <eyw04@users.noreply.github.com> Co-authored-by: yshh00 <yshh00@users.noreply.github.com>

--- public/popup.js
+++ public/popup.js
@@ -1,6 +1,3 @@
-import parseCurrency from 'parsecurrency';
-import countryToCurrency from "country-to-currency";
-
 document.addEventListener('DOMContentLoaded', () => {
   const toCurrency = document.getElementById('toCurrency');
   const convertBtn = document.getElementById('convertBtn');
@@ -30,37 +27,55 @@ priceScrapper.addEventListener("click", async () => {
   });
 })
 
-// Function to get text content of a span element with class "hello"
-function getSpanText() {
-  let wholePrice = document.querySelectorAll('span._cDEzb_p13n-sc-price_3mJ9Z');
-  // if (wholePrice) {
-  //     const nestedElement = wholePrice.querySelector('.a-price-decimal');
-  //     if (nestedElement) {
-  //       nestedElement.remove();
-  //     }
+var currencyMatcher = /^(?:([-+]{1}) ?)?(?:([A-Z]{3}) ?)?(?:([^\d ]+?) ?)?(((?:\d{1,3}([,. ’'\u00A0\u202F]))*?\d{1,})(([,.])\d{1,2})?)(?: ?([^\d]+?))??(?: ?([A-Z]{3}))?$/;
+var gr = /^\d{1,3}([,. ’'\u00A0\u202F]\d{3})*$/; // validate groups
+var ind = /^\d{1,2}(,\d{2})*(,\d{3})?$/; // exception for Indian number format
+
+function parseCurrency(priceStr) {
+  // if (!priceStr || !priceStr.match) return null;
+  priceStr = priceStr.trim();
+  var match = priceStr.match(currencyMatcher);
+  console.log(match);
+  // if (!match) return null;
+  var groupSeparator = match[6] || '';
+  var decimalSeparator = match[8] || '';
+  // if (groupSeparator === decimalSeparator && decimalSeparator) {
+  //   return null;
   // }
-  // let fractionPrice = document.querySelector('span.a-price-fraction');
-  // alert(wholePrice.innerHTML + " " + fractionPrice.innerHTML);
-
-
-  // get the exchange rate
-
-
-
-  for (let i = 0; i < wholePrice.length; i++) {
-    console.log(wholePrice[i].innerHTML);
-    console.log(getCurrencyInfo(wholePrice[i].innerHTML));
-
-    wholePrice[i].innerHTML = "123";
+  var integer = match[1] === '-' ? '-' + match[5] : match[5];
+  // if (groupSeparator && !match[5].match(gr) && !match[5].match(ind)) {
+  //   return null;
+  // }
+  var value = match[4];
+  // if (!value) return null;
+  if (groupSeparator) {
+    value = value.replace(RegExp('\\' + groupSeparator, 'g'), '');
   }
-
+  if (decimalSeparator) {
+    value = value.replace(decimalSeparator, '.');
+  }
+  var numericVal = match[1] === '-' ? value * -1 : +value;
+  // if (typeof numericVal !== 'number' || isNaN(numericVal)) {
+  //   return null;
+  // }
+  return {
+    raw: priceStr,
+    value: numericVal,
+    integer: integer || '',
+    decimals: match[7] || '',
+    currency: match[2] || match[10] || '',
+    symbol: match[3] || match[9] || '',
+    decimalSeparator: decimalSeparator,
+    groupSeparator: groupSeparator,
+    sign: match[1] || ''
+  };
 };
 
 // Method get currency info from string
 function getCurrencyInfo(price_string) {
   var info = parseCurrency(price_string);
 
-  // If currency code is not present in price, use website country code
+  // // If currency code is not present in price, use website country code
   // if (info.currency == "") {
   //   // Checks for sites of type (https://amazon.de, https://amazon.co.uk)
   //   var country_code = location.host.replace(/^.*\./, "");
@@ -80,3 +95,32 @@ function getCurrencyInfo(price_string) {
 
   return info;
 }
+
+// Function to get text content of a span element with class "hello"
+function getSpanText() {
+  let wholePrice = document.querySelectorAll('span._cDEzb_p13n-sc-price_3mJ9Z');
+
+
+
+  // if (wholePrice) {
+  //     const nestedElement = wholePrice.querySelector('.a-price-decimal');
+  //     if (nestedElement) {
+  //       nestedElement.remove();
+  //     }
+  // }
+  // let fractionPrice = document.querySelector('span.a-price-fraction');
+  // alert(wholePrice.innerHTML + " " + fractionPrice.innerHTML);
+
+
+  // get the exchange rate
+
+
+  for (let i = 0; i < wholePrice.length; i++) {
+    console.log(wholePrice[i].innerHTML);
+    console.log(getCurrencyInfo(wholePrice[i].innerHTML));
+
+
+    // wholePrice[i].innerHTML = "123";
+  }
+
+};
